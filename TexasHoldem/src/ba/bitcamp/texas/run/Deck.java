4e96b679b6864022ccf1ea9ec64f8fa48b60127c
ognjencetkovic <ognjen.cetkovic@bitcamp.ba>
2015-08-11 23:35:29 +0200

    HandEvaluator added

--- TexasHoldem/src/ba/bitcamp/texas/run/Deck.java
+++ TexasHoldem/src/ba/bitcamp/texas/run/Deck.java
@@ -1,6 +1,7 @@
 package ba.bitcamp.texas.run;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.TreeSet;
 
@@ -25,22 +26,66 @@ public class Deck {
 	
 	public static void main(String[] args) {
 		Deck d = new Deck();
-		System.out.println(d.deck);
+		//System.out.println(d.deck);
 		d.shuffle();
-		System.out.println(d.deck);
+		System.out.println(d.deck + "\n");
 		
+		ArrayList<Card> table = new ArrayList<Card>();
 		for (int i = 0; i < 5; i++) {
-			System.out.print(d.deck.remove(i) + ", ");
-		}	
-		System.out.println();
-		for (int j = 0; j < 4; j++) {
-			System.out.print("player " + (j + 1) + ": ");
-			for (int i = 0; i < 2; i++) {
-				System.out.print(d.deck.remove(i) + ", ");
-			}			
-			System.out.println();
+			table.add(d.deck.remove(i));
 		}
-		System.out.println(d.deck);
+		System.out.println("table: " + table + "\n");
+		
+		ArrayList<Card> p1 = new ArrayList<Card>(table);
+		for (int i = 0; i < 2; i++) {
+			p1.add(d.deck.remove(i));
+		}
+		System.out.println("p1: " + p1 + "\n");
+		ArrayList<Card> p2 = new ArrayList<Card>(table);
+		for (int i = 0; i < 2; i++) {
+			p2.add(d.deck.remove(i));
+		}
+		System.out.println("p2: " + p2 + "\n");
+		ArrayList<Card> p3 = new ArrayList<Card>(table);
+		for (int i = 0; i < 2; i++) {
+			p3.add(d.deck.remove(i));
+		}
+		System.out.println("p3: " + p3 + "\n");
+		ArrayList<Card> p4 = new ArrayList<Card>(table);
+		for (int i = 0; i < 2; i++) {
+			p4.add(d.deck.remove(i));
+		}
+		System.out.println("p4: " + p4 + "\n");
+		
+		HandEvaluator2 h1 = new HandEvaluator2(p1);
+		HandEvaluator2 h2 = new HandEvaluator2(p2);
+		HandEvaluator2 h3 = new HandEvaluator2(p3);
+		HandEvaluator2 h4 = new HandEvaluator2(p4);
+		
+		HandEvaluator2[] h = new HandEvaluator2[4];
+		h[0] = h1;
+		h[1] = h2;
+		h[2] = h3;
+		h[3] = h4;
+		Arrays.sort(h);
+		for (HandEvaluator2 handEvaluator2 : h) {
+			System.out.println(handEvaluator2.hand);
+		}
+		
+		
+		
+//		for (int i = 0; i < 5; i++) {
+//			System.out.print(d.deck.remove(i) + ", ");
+//		}	
+//		System.out.println();
+//		for (int j = 0; j < 4; j++) {
+//			System.out.print("player " + (j + 1) + ": ");
+//			for (int i = 0; i < 2; i++) {
+//				System.out.print(d.deck.remove(i) + ", ");
+//			}			
+//			System.out.println();
+//		}
+//		System.out.println(d.deck);
 		
 	}
 }
--- TexasHoldem/src/ba/bitcamp/texas/run/HandEvaluator2.java
+++ TexasHoldem/src/ba/bitcamp/texas/run/HandEvaluator2.java
@@ -3,7 +3,6 @@ package ba.bitcamp.texas.run;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import ba.bitcamp.texas.run.Card.Suit;
 import ba.bitcamp.texas.run.Card.Value;
@@ -12,7 +11,8 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 
 	private static final int MAX_NUM_OF_PAIRS = 2;
 	
-	private ArrayList<Card> hand;
+	//TODO
+	public ArrayList<Card> hand;
 	
 	private int[] distibutionOfValues = new int[Value.values().length];
 	private int[] distibutionOfSuits = new int[Suit.values().length];
@@ -23,11 +23,13 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 	private int[] pairs = new int[MAX_NUM_OF_PAIRS];
 
 	private int straightRank;
+	private int straightFlushRank;
+	private Suit flushSuit;
 
 	public HandEvaluator2(ArrayList<Card> hand) {
 		this.hand = hand;
-
-		
+		calculateDistributions();
+		findDuplicates();
 	}
 
 	private void calculateDistributions() {
@@ -43,6 +45,7 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 			if (distibutionOfValues[i] != 0) {
 				counter++;
 				if (counter >= 5){
+					straightRank = distibutionOfValues[i];
 					return true;
 				}
 			} else {
@@ -51,6 +54,7 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 		}
 		if (counter == 4) {
 			if (distibutionOfValues[distibutionOfValues.length - 1] != 0)
+				straightRank = Value.ACE.ordinal();
 				return true;
 		}
 		return false;
@@ -59,6 +63,7 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 	private boolean hasFlush() {
 		for (int i = 0; i < distibutionOfSuits.length; i++) {
 			if (distibutionOfSuits[i] >= 5) {
+				flushSuit = Suit.values()[i];
 				return true;
 			}
 		}
@@ -81,7 +86,6 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
 	
 	private boolean hasStraightFlush() {
         if (hasStraight() && hasFlush()) {
-            // Flush and Straight (possibly separate); check for Straight Flush.
         	TreeMap<Suit, ArrayList<Value>> set = new TreeMap<>();
         	for (Suit s : Suit.values()){
         		set.put(s, new ArrayList<Card.Value>());
@@ -89,43 +93,189 @@ public class HandEvaluator2 implements Comparable<HandEvaluator2> {
         	for (Card card : hand) {
         		set.get(card.getSuit()).add(card.getValue());
         	}
-        	
         	for (Suit s : Suit.values()){
         		if (set.get(s).size() >= 5){
         			int counter = 0;
         			ArrayList<Value> tmp = set.get(s);
         			Collections.sort(tmp);
-        			for (int i = 0; i < tmp.size() - 1; i++) {
-        				if (tmp.get(i).ordinal() + 1 == tmp.get(i + 1).ordinal()) {
+        			for (int i = tmp.size() - 1; i >= 1; i--) {
+        				if (tmp.get(i).ordinal() - 1 == tmp.get(i - 1).ordinal()) {
         					counter++;
         					if (counter >= 4){
+        						straightFlushRank = tmp.get(i - 1).ordinal();
         						return true;
         					}
         				} else {
         					counter = 0;
         				}
         			}
-        			if (counter == 4) {
-        				if (distibutionOfValues[distibutionOfValues.length - 1] != 0)
-        					return true;
+        			if (counter == 3 && tmp.get(tmp.size() - 1).equals(Value.ACE)) {
+        				return true;
         			}
-        			return false;
         		}
         	}
-         	
         }
+        return false;
 	}
-	
 
 	@Override
 	public int compareTo(HandEvaluator2 o) {
-		
-		
-		
-		
-		
+		//straight flush
+		if (this.compareStraightFlush(o) != null) {
+			return this.compareStraightFlush(o);
+		}
+		//poker
+		if (this.compareFourOfAKind(o) != null) {
+			return this.compareFourOfAKind(o);
+		}
+		//full house
+		if (this.compareFullHouse(o) != null) {
+			return this.compareFullHouse(o);
+		}
+		//flush
+		if (this.compareFlush(o) != null) {
+			return this.compareFlush(o);
+		}
+		//straight
+		if (this.compareStraight(o) != null) {
+			return this.compareStraight(o);
+		}
+		//three of a kind
+		if (this.compareThreeOfAKind(o) != null) {
+			return this.compareThreeOfAKind(o);
+		}
+		//two pairs
+		if (this.compareTwoPairs(o) != null) {
+			return this.compareTwoPairs(o);
+		}
+		//pair
+		if (this.comparePairs(o) != null) {
+			return this.comparePairs(o);
+		}
+		//high card
 		
 		return 0;
+	}
+
+	private Integer comparePairs(HandEvaluator2 o) {
+		if (this.numOfPairs == 1) {
+			if (o.numOfPairs == 1) {
+				//TODO veca karta
+				System.out.println("pair");
+			}
+		}
+		return null;
+	}
+
+	private Integer compareTwoPairs(HandEvaluator2 o) {
+		if (this.numOfPairs == 2) {
+			if(o.numOfPairs == 2) {
+				//TODO
+				System.out.println("two pairs");
+			} else 
+				return -1;
+		} else if (o.numOfPairs == 2) {
+			return 1;
+		}
+		return null;
+	}
+
+	private Integer compareThreeOfAKind(HandEvaluator2 o) {
+		if (this.threeOfAKind != 0) {
+			if (o.threeOfAKind != 0) {
+				return o.threeOfAKind - this.threeOfAKind;
+			} else {
+				return -1;
+			}
+		} else if (o.threeOfAKind != 0) {
+			return 1;
+		}
+		return null;
+	}
+
+	private Integer compareStraight(HandEvaluator2 o) {
+		if (this.hasStraight()) {
+			//TODO
+			System.out.println("straight");
+		}
+		return null;
+	}
+
+	private Integer compareFlush(HandEvaluator2 o) {
+		if (this.hasFlush()) {
+			if(o.hasFlush()){
+				Collections.sort(this.hand);
+				Collections.sort(o.hand);
+				for (int i = this.hand.size() - 1; i >= 0; i--) {
+					if(this.hand.get(i).getSuit().equals(this.flushSuit)){
+						for (int j = o.hand.size() - 1; j >= 0; j--) {
+							if(o.hand.get(j).getSuit().equals(o.flushSuit)){
+								return o.hand.get(j).getValue().ordinal() - this.hand.get(i).getValue().ordinal(); 
+							}
+						}
+					}
+				}
+				return 0;
+			}
+			return -1;
+		} else if (o.hasFlush()) {
+			return 1;
+		}
+		return null;
+	}
+
+	private Integer compareFullHouse(HandEvaluator2 o) {
+		if (this.threeOfAKind != 0 && this.numOfPairs != 0) {
+			if (this.threeOfAKind != 0 && this.numOfPairs != 0) {
+				if (this.threeOfAKind == o.threeOfAKind) {
+					//TODO
+					System.out.println("full house");
+				} else {
+					return o.threeOfAKind - this.threeOfAKind;
+				}
+			}
+		} else if (o.threeOfAKind != 0 && o.numOfPairs != 0) {
+			return 1;
+		}
+		return null;
+	}
+
+	private Integer compareFourOfAKind(HandEvaluator2 o) {
+		if (this.fourOfAKind != 0) {
+			if (o.fourOfAKind != 0) {
+				if (o.fourOfAKind == this.fourOfAKind){
+					Collections.sort(this.hand);
+					Collections.sort(o.hand);
+					return o.hand.get(o.hand.size() - 1).getValue().ordinal() - this.hand.get(this.hand.size() - 1).getValue().ordinal();
+				}
+				return o.fourOfAKind - this.fourOfAKind;
+			} else {
+				return -1;
+			}
+		} else if (o.fourOfAKind != 0) {
+			return 1;
+		}
+		return null;
+	}
+
+	private Integer compareStraightFlush(HandEvaluator2 o) {
+		if (this.hasStraightFlush()) {
+			if (o.hasStraightFlush()) {
+				if(this.straightFlushRank == Value.ACE.ordinal()) {
+					if (o.straightFlushRank == Value.ACE.ordinal())
+						return 0;
+					else 
+						return 1;
+				} else if (o.straightFlushRank == Value.ACE.ordinal()) 
+					return -1;
+				return o.straightFlushRank - this.straightFlushRank;
+			} else {
+				return -1;
+			}
+		} else if (o.hasStraightFlush()) {
+			return 1;
+		}
+		return null;
 	}
 	
 }
